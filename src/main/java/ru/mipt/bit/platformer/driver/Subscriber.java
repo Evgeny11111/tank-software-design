e6bb8251da5e876f22bf1c1adf8f143cf935f030
Evgeny11111 <81196524+Evgeny11111@users.noreply.github.com>
2021-12-12 20:26:34 +0300

    Delete extra code

--- src/main/java/ru/mipt/bit/platformer/driver/Subscriber.java
+++
@@ -1,8 +0,0 @@
-package ru.mipt.bit.platformer.driver;
-
-import ru.mipt.bit.platformer.objects.ObjectByGame;
-import ru.mipt.bit.platformer.objects.Event;
-
-public interface Subscriber {
-    void update(Event event, ObjectByGame objectByGame);
-}
