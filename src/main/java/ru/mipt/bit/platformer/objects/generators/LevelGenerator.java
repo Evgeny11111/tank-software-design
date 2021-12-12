760833f2175d0be4100d231bc3f4697a356e8f43
Evgeny11111 <81196524+Evgeny11111@users.noreply.github.com>
2021-12-12 20:28:35 +0300

    Delete extra code

--- src/main/java/ru/mipt/bit/platformer/objects/generators/LevelGenerator.java
+++
@@ -1,8 +0,0 @@
-package ru.mipt.bit.platformer.objects.generators;
-
-import ru.mipt.bit.platformer.driver.Driver;
-import ru.mipt.bit.platformer.driver.Level;
-
-public interface LevelGenerator {
-    Level generateLevel();
-}
